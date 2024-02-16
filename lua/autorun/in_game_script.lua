0a104093b5457ac87c5ec131c609d40ef269fb9d
ZIPPY <simperzippy@gmail.com>
2024-02-16 21:55:14 +0100

    lots o shid

--- lua/autorun/in_game_script.lua
+++ lua/autorun/in_game_script.lua
@@ -1,8 +1,31 @@
--- https://wiki.facepunch.com/gmod/Global.RunString
+
+
+if file.Exists("inscautorun.txt", "DATA") then
+    RunString( file.Read("inscautorun.txt"), "InGameScript_Server", false )
+end
+
+
+
+if SERVER then
+
+    util.AddNetworkString("INSC_Server")
+
+    net.Receive("INSC_Server", function()
+
+        local msg = RunString( net.ReadString(), "InGameScript_Server", false )
+
+        if msg then
+            PrintMessage(HUD_PRINTTALK, msg)
+        end
+
+    end)
+
+end
+
 
 if CLIENT then
 
-    local function INSC( text, close, endfunc )
+    local function INSC( title, text, close, endfunc, forcerealm )
         
         if !LocalPlayer():IsSuperAdmin() then return end
 
@@ -11,35 +34,58 @@ if CLIENT then
 
 
         local notepadFrame = vgui.Create("DFrame")
+        local executeButton = vgui.Create("DButton", notepadFrame)
+        local codeInput = vgui.Create("DTextEntry", notepadFrame)
+        local RealmDropdown = vgui.Create("DComboBox", notepadFrame)
+
         notepadFrame:SetSize(ScrW()*0.85, ScrH()*0.85)
-        notepadFrame:SetTitle("In-game Script Editor")
+        notepadFrame:SetTitle(title)
         notepadFrame:Center()
         notepadFrame:MakePopup()
 
-        local RealmDropdown = vgui.Create("DComboBox", notepadFrame)
-        RealmDropdown:Dock(TOP)
-        RealmDropdown:AddChoice("Server")
-        RealmDropdown:AddChoice("Client")
-        RealmDropdown:AddChoice("Shared")
-        RealmDropdown:ChooseOption("Shared")
+        if !forcerealm then
+            RealmDropdown:Dock(TOP)
+            RealmDropdown:AddChoice("Server")
+            RealmDropdown:AddChoice("Client")
+            RealmDropdown:AddChoice("Shared")
+            RealmDropdown:ChooseOption("Shared")
+        end
 
-        local codeInput = vgui.Create("DTextEntry", notepadFrame)
         codeInput:Dock(FILL)
         codeInput:SetMultiline(true)
         codeInput:SetFont("TargetID")
         codeInput:SetTextColor(Color(180, 180, 160))
         codeInput:SetText(text)
+        function codeInput:OnChange()
 
-        local executeButton = vgui.Create("DButton", notepadFrame)
+            local inptext = codeInput:GetText()
+
+            if inptext && #inptext >= 1 then
+                executeButton:SetEnabled(true)
+            else
+                executeButton:SetEnabled(false)
+            end
+
+        end
+
         executeButton:Dock(BOTTOM)
         executeButton:SetText("Execute Code")
-
-
+        executeButton:SetEnabled(#text >= 1)
         function executeButton:DoClick()
-            local msg = RunString( codeInput:GetText(), "InGameScript", false )
-    
-            if msg then
-                chat.AddText(Color(255, 0, 0), msg)
+            local realm = forcerealm or RealmDropdown:GetSelected() or "Shared"
+
+            if realm == "Client" or realm == "Shared" then
+                local msg = RunString( codeInput:GetText(), "InGameScript", false )
+
+                if msg then
+                    chat.AddText(Color(255, 0, 0), msg)
+                end
+            end
+
+            if realm == "Server" or realm == "Shared" then
+                net.Start("INSC_Server")
+                net.WriteString(codeInput:GetText())
+                net.SendToServer()
             end
 
             if close then
@@ -51,15 +97,24 @@ if CLIENT then
             end
         end
 
+
     end
 
+
+
     local function GetINSCAutorun()
-        
+
+        if !file.Exists("inscautorun.txt", "DATA") then
+            file.Write("inscautorun.txt", 'print("sup")')
+        end
+
+        return file.Read("inscautorun.txt")
+
     end
 
 
     local function SaveINSCAutoRun( text )
-        
+        file.Write("inscautorun.txt", text)
     end
 
 
@@ -68,10 +123,10 @@ if CLIENT then
         if ply==LocalPlayer() then
 
             if string.lower(text) == "!insc" then
-                INSC( _, true )
+                INSC( "In-game Script", _, true )
                 return true
             elseif string.lower(text) == "!ainsc" then
-                INSC( GetINSCAutorun(), true, SaveINSCAutoRun )
+                INSC( "In-game Autorun", GetINSCAutorun(), true, SaveINSCAutoRun, "Shared" )
                 return true
             end
         end
--- todo.md
+++ todo.md
@@ -1,3 +1,6 @@
 # TODO
-- [ ] A dropdown with client, server, and shared as alternatives
-- [ ] A second command that opens an autorun editor, should be saved in data folder
+- [x] A dropdown with client, server, and shared as alternatives
+- [x] A second command that opens an autorun editor, should be saved in data folder
+- [ ] Tab should add a tab
+- [ ] Control+s runs the code and closes the menu, saves if it is autorun
+- [ ] Autorun error message in chat on boot
